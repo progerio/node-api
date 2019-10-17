a6f67866538b35ac8d1b16e072c96da16476bffa
Paulo Rogerio R Araujo <paulo.rogeriobr@gmail.com>
2019-10-17 14:44:59 -0300

    Alterando usuario do banco

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -7,16 +7,16 @@ module.exports = {
   // }, 
 
   development: {
-    username: "root",
-    password: 123456,
+    username: "postgres",
+    password: '',
     database: "books",
     host: "127.0.0.1",
     dialect: "postgres"
     
   },
   test: {
-    username: "root",
-    password: 123456,
+    username: "postgres",
+    password: '',
     database: "books_test",
     host: "127.0.0.1",
     dialect: "postgres"
